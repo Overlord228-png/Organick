0f38d216a761e1f66ae2df8ff186f1db61e44048
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2024-08-06 15:50:24 +0300

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
 
 const App: React.FC = () => {
   return (
     <Routes>
-      <Route />
+      <Route path='/' element={<Layout/>}>
+        <Route index element={<Home/>}/>
+      </Route>
     </Routes>
   );
 }
