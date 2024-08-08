691f24c5d716363ba61119706aed97c2aeb47dd3
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2024-08-08 14:02:20 +0300

    add Route for pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
+import About from './pages/About';
+import Shop from './pages/Shop';
+import Projects from './pages/Projects';
+import News from './pages/News';
+import Pages from './pages/Pages';
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route path='/' element={<Layout/>}>
         <Route index element={<Home/>}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/pages' element={<Pages />}/>
+        <Route path='/shop' element={<Shop />}/>
+        <Route path='/projects' element={<Projects />}/>
+        <Route path='/news' element={<News />}/>
       </Route>
     </Routes>
   );
