93e607f46f6fb475b2f96db2dcd52bf67a28066d
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2024-08-08 14:01:01 +0300

    add jump to pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,31 +8,33 @@ const Header: React.FC = () => {
 	return (
 		<header className="header">
 			<div className="header_block">
-				<div className="logoContainer">
-					<img src={Logo} alt="" className="logo" />
-					<h1 className="title">Organick</h1>
+				<div className="header_block-left">
+					<div className="logoContainer">
+						<img src={Logo} alt="" className="logo" />
+						<h1 className="title">Organick</h1>
+					</div>
+					<ul className="navLinks">
+						<NavLink to={""} className="navLink">
+							Home
+						</NavLink>
+						<NavLink to={"/About"} className="navLink">
+							About
+						</NavLink>
+						<NavLink to={"/Pages"} className="navLink">
+							Pages
+						</NavLink>
+						<NavLink to={"/Shop"} className="navLink">
+							Shop
+						</NavLink>
+						<NavLink to={"/Projects"} className="navLink">
+							Projects
+						</NavLink>
+						<NavLink to={"/News"} className="navLink">
+							News
+						</NavLink>
+					</ul>
 				</div>
-				<ul className="navLinks">
-					<NavLink to={""} className="navLink">
-						<p>Home</p>
-					</NavLink>
-					<NavLink to={""} className="navLink">
-						<p>About</p>
-					</NavLink>
-					<NavLink to={""} className="navLink">
-						<p>Pages</p>
-					</NavLink>
-					<NavLink to={""} className="navLink">
-						<p>Shop</p>
-					</NavLink>
-					<NavLink to={""} className="navLink">
-						<p>Projects</p>
-					</NavLink>
-					<NavLink to={""} className="navLink">
-						<p>News</p>
-					</NavLink>
-				</ul>
-				<div className="">
+				<div className="header_block-right">
 					<InputSearch />
 					<BtnCase count={0}/>
 				</div>
