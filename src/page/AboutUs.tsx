69622e9f91c8c793c3c8083845214e312395ff6b
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2024-09-27 20:17:08 +0300

    add page about

---
+++ src/page/AboutUs.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AboutUs: React.FC = () => {
+    return (
+        <>
+            <div className="page-banner page-banner_about-us">
+                <h1 className="page-banner__text">About Us</h1>
+            </div>
+            <section>AboutUs</section>
+        </>
+    )
+}
+
+export default AboutUs
