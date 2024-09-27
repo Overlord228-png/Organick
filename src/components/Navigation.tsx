7aa2d5b2a3571032832ddf99eb506f1f8e5f2d56
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2024-09-27 20:16:25 +0300

    add navigator

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,16 @@
+import { useSelector } from "react-redux";
+
+const Navigation: React.FC = () => {
+    // const toggleShoppingCart = useSelector();
+    // const visibleInput = useSelector(selectVisibleInput);
+
+    return (
+        <>
+            <div
+                className={""}
+            ></div>
+        </>
+    );
+};
+
+export default Navigation;
