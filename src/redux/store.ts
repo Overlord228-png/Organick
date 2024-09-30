4e82ecd13d304c075ea841947e45f94933845ec6
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2024-09-30 11:20:10 +0300

    update

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-const store = configureStore({
+import shop from "./slices/shopSlice";
+import cart from "./slices/cartSlice";
+import input from "./slices/inputSlice";
+import posts from "./slices/postsSlice";
+import singleProduct from "./slices/singleProductSlice";
+import singlePortfolio from "./slices/singlePortfolioSlice";
+import catalogue from "./slices/catalogueSlice";
+import singlePost from "./slices/singlePostSlice";
+import authorsPosts from "./slices/authorsPostsSlice";
+
+export const store = configureStore({
+  // Reducer -> Connect the function
   reducer: {
-    // your reducers here
+    shop,
+    cart,
+    input,
+    posts,
+    singleProduct,
+    singlePortfolio,
+    catalogue,
+    singlePost,
+    authorsPosts,
   },
 });
 
-export { store };
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
